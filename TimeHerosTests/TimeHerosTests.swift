3ad4790aa969113583968de62f825d42c0d9b317
Victor Cotrim <cotrim149@gmail.com>
2018-05-05 16:42:01 -0300

    Initial Commit

---
+++ TimeHerosTests/TimeHerosTests.swift
@@ -0,0 +1,36 @@
+//
+//  TimeHerosTests.swift
+//  TimeHerosTests
+//
+//  Created by Victor de Lima on 05/05/2018.
+//  Copyright © 2018 Victor Cotrim. All rights reserved.
+//
+
+import XCTest
+@testable import TimeHeros
+
+class TimeHerosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
