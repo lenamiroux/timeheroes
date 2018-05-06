03286e59e84a6cde1579ea55dba06601c383891b
Victor Cotrim <cotrim149@gmail.com>
2018-05-06 05:03:54 -0300

    Adding date cell

--- TimeHeros/Controller/Destiny/DestinyViewController.swift
+++ TimeHeros/Controller/Destiny/DestinyViewController.swift
@@ -10,25 +10,69 @@ import UIKit
 
 class DestinyViewController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var tableView: UITableView!
+	override func viewDidLoad() {
         super.viewDidLoad()
+		registerCell()
+		setupNavigation()
+    }
+	
+	func setupNavigation() {
 		navigationItem.title = "Destino"
-
 		navigationController?.navigationBar.prefersLargeTitles = true
 		navigationController?.navigationBar.tintColor = UIColor(red: 144.0 / 255.0, green: 19.0 / 255.0, blue: 254.0 / 255.0, alpha: 1.0)
+	}
+}
 
-    }
+extension DestinyViewController : UITableViewDelegate, UITableViewDataSource{
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		switch indexPath.row {
+		case 0:
+			return DateCell.cellHeight
+		default:
+			return 0
+		}
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		switch indexPath.row {
+		case 0:
+			return generateDateCell()
+		default:
+			return UITableViewCell()
+		}
+	}
 	
 	
+}
 
-    /*
-    // MARK: - Navigation
+extension DestinyViewController {
+	
+	func registerCell() {
+		tableView.register(UINib(nibName: "DateCell", bundle: Bundle.main), forCellReuseIdentifier: DateCell.reuseIdentifier)
+	}
+	
+	func generateDateCell() -> DateCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: DateCell.reuseIdentifier) as! DateCell
+		
+		let now = Date()
+		let dateFormmater = DateFormatter()
+		dateFormmater.dateFormat = "EEEE, d MMMM yyyy - HH:mm"
+		dateFormmater.locale = Locale(identifier: "PT_br")
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+		let date = dateFormmater.string(from: now)
+		cell.dateLabel.text = date
+		
+		return cell
+	}
 }
